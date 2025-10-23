84bd2d4b7a806801e0dd3c158ed70cc70dbfbc22
Yamini Priya <129665645+kyaminipriya9423@users.noreply.github.com>
2025-10-23 23:55:24 +0530

    backend deployment is done

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://127.0.0.1:8000";
+const API_BASE = "https://me-api-playground-as54.onrender.com";
 
 // ---------------- Profile ----------------
 fetch(`${API_BASE}/profile`)
